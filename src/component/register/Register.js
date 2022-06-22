cba0ae1683e0114207f77d0eb5915a086b505aba
Simion Gitau <simiongitau33@gmail.com>
2022-06-22 13:39:43 +0300

    progress

--- src/component/register/Register.js
+++ src/component/register/Register.js
@@ -37,42 +37,42 @@ export default function Register() {
       <div className="register_container flex flex-col bg-[#c8eaf4] w-[500px] h-fit p-4">
         <h4 className="mb-4">register</h4>
 
-        <div className="flex flex-col my-2">
+        <div className="flex flex-col my-2 w-full">
           <label className="text-[20px]">name</label>
           <input
             required
             onChange={(e) => setName(e.target.value)}
-            className=" p-2 w-full"
+            className=" p-2"
           />
         </div>
 
-        <div className="flex flex-col my-2">
+        <div className="flex flex-col my-2 w-full">
           <label className="text-[20px]">email</label>
           <input
             required
             onChange={(e) => setEmail(e.target.value)}
             type="email"
-            className=" p-2 w-full"
+            className=" p-2"
           />
         </div>
 
-        <div className="flex flex-col my-2">
+        <div className="flex flex-col my-2 w-full">
           <label className="text-[20px]"> password</label>
           <input
             required
             type="password"
             onChange={(e) => setPassword(e.target.value)}
-            className=" p-2 w-full"
+            className=" p-2"
           />
         </div>
 
-        <div className="flex flex-col my-2">
+        <div className="flex flex-col my-2 w-full">
           <label className="text-[20px]">confirm password</label>
           <input
             required
             onChange={(e) => setconfirm(e.target.value)}
             type="password"
-            className=" p-2 w-full"
+            className=" p-2"
           />
         </div>
 
